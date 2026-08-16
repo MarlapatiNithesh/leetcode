44f7b6beec0208a85a80a7254507f51ee1f91144
Marlapati Nithesh <marlapati.2023ug1112@iiitranchi.ac.in>
2026-08-16 23:25:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (88.44%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-       int n=nums.size();
-       if(n==1)return nums[0];
-       vector<int>dp1(n+1,-1),dp2(n+1,-1);
-       auto dfs=[&](auto& slef,int i,int en,vector<int>&dp)->int{
-          if(i>en)return 0;
-          if(dp[i]!=-1)return dp[i];
-          dp[i]=max(nums[i]+slef(slef,i+2,en,dp),slef(slef,i+1,en,dp));
-          return dp[i];
-       };
-       return max(dfs(dfs,0,n-2,dp1),dfs(dfs,1,n-1,dp2));
+        int n=nums.size();
+        if(n==1)return nums[0];
+
+        auto dfs=[&](int l,int r)->int{
+            int a=0,b=0;
+            for(int i=l;i<=r;i++){
+                int c=max(nums[i]+a,b);
+                a=b;
+                b=c;
+            }
+            return b;
+        };
+
+        return max(dfs(0,n-2),dfs(1,n-1));
     }
 };
