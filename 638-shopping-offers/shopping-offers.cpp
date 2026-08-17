8841ff154f0b4cab50c3fbaaf2461baf8546bf7c
Marlapati Nithesh <marlapati.2023ug1112@iiitranchi.ac.in>
2026-08-17 12:05:32 +0530

    Time: 39 ms (27.99%) | Memory: 29.2 MB (9.82%) - LeetSync

--- 638-shopping-offers/shopping-offers.cpp
+++ 638-shopping-offers/shopping-offers.cpp
@@ -2,15 +2,22 @@ class Solution {
 public:
     int shoppingOffers(vector<int>& prices,vector<vector<int>>& special,vector<int>& req){
         int n=prices.size();
+        map<pair<int,vector<int>>,int>dp;
 
-        auto dfs=[&](auto& self,int idx,int s)->int{
+        auto dfs=[&](auto& self,int idx,vector<int> req)->int{
             if(idx>=special.size()){
+                int s=0;
                 for(int i=0;i<n;i++)
                     s+=req[i]*prices[i];
                 return s;
             }
 
-            int notTake=self(self,idx+1,s);
+            pair<int,vector<int>>key={idx,req};
+
+            if(dp.count(key))
+                return dp[key];
+
+            int notTake=self(self,idx+1,req);
 
             bool ok=true;
             for(int i=0;i<n;i++){
@@ -19,20 +26,19 @@ public:
                     break;
                 }
             }
+
             int take=INT_MAX;
+
             if(ok){
                 for(int i=0;i<n;i++)
                     req[i]-=special[idx][i];
 
-                take=self(self,idx,s+special[idx][n]);
-                for(int i=0;i<n;i++){
-                    req[i]+=special[idx][i];
-                }
-
+                take=self(self,idx,req)+special[idx][n];
             }
-            return min(take,notTake);
+
+            return dp[key]=min(take,notTake);
         };
 
-        return dfs(dfs,0,0);
+        return dfs(dfs,0,req);
     }
 };
