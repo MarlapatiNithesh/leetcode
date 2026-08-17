307d90f4d7113bf3d9e390eac34673096f444ab4
Marlapati Nithesh <marlapati.2023ug1112@iiitranchi.ac.in>
2026-08-17 19:58:33 +0530

    Time: 303 ms (5.15%) | Memory: 263.2 MB (8.26%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     int minSteps(int n) {
-        vector<vector<int>>dp(1001,vector<int>(2,1e9));
-        auto dfs=[&](auto& slef,int prev,int tt,int la)->int{
-            if(tt==n)return 0;
-            // if(dp[prev][la]!=-1)return dp[prev][la];
-            int cp=1e9,pa=1e9;
-            if(la){
-                cp=1+slef(slef,tt,tt,0);
-            }
-            if(prev>0&&tt+prev<=n){
-                pa=1+slef(slef,prev,tt+prev,1);
-            }
-            return min(cp,pa);
+        const int INF = 1e9;
+        // dp[c][s][la] -> flat array, avoids nested vector allocation overhead
+        vector<int> dp((n + 1) * (n + 1) * 2, INF);
+        auto idx = [&](int c, int s, int la) {
+            return (c * (n + 1) + s) * 2 + la;
         };
 
-        return dfs(dfs,0,1,1);
-        // int tt=0;
-        // dp[n][0]=dp[n][1]=0;
-        // for(int prev=n;i>=0;i--){
-        //     for(int la=0;la<2;la++){
-        //         dp[prev][la]=min(dp[prev+tt][la],dp[prev][la]);
-        //     }
-        // }
+        for (int c = 0; c <= n; c++) {
+            dp[idx(c, n, 0)] = 0;
+            dp[idx(c, n, 1)] = 0;
+        }
 
+        for (int s = n - 1; s >= 1; s--) {
+            for (int la = 0; la < 2; la++) {
+                int cp = INF, pa = INF;
+                if (la) cp = 1 + dp[idx(s, s, 0)];
+                if (2 * s <= n) pa = 1 + dp[idx(s, 2 * s, 1)];
+                dp[idx(s, s, la)] = min(cp, pa);
+            }
+            for (int c = 0; c <= n; c++) {
+                if (c == s) continue;
+                for (int la = 0; la < 2; la++) {
+                    int cp = INF, pa = INF;
+                    if (la) cp = 1 + dp[idx(s, s, 0)];
+                    if (c > 0 && s + c <= n) pa = 1 + dp[idx(c, s + c, 1)];
+                    dp[idx(c, s, la)] = min(cp, pa);
+                }
+            }
+        }
 
+        return dp[idx(0, 1, 1)];
     }
 };
