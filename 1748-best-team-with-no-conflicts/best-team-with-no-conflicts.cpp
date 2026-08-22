945fb6005e1d5d4f2dcab2476e8b79e0cdb0d627
Marlapati Nithesh <marlapati.2023ug1112@iiitranchi.ac.in>
2026-08-22 19:23:42 +0530

    Time: 21 ms (90.95%) | Memory: 24.5 MB (65.19%) - LeetSync

--- 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.cpp
+++ 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-        vector<pair<int,int>> pa;
-
-        for(int i = 0; i < scores.size(); i++){
-            pa.push_back({ages[i], scores[i]});
-        }
-
-        sort(pa.begin(), pa.end());
-
-        vector<int> sc;
-
-        for(auto it : pa){
-            sc.push_back(it.second);
-        }
-
-        vector<int> a;
-
-        for(int i = 0; i < scores.size(); i++){
-            a.push_back(sc[i]);
-
-            for(int j = 0; j < i; j++){
-                if(sc[j] <= sc[i]){
-                    a[i] = max(a[i], a[j] + sc[i]);
+        int n=scores.size();
+        vector<pair<int,int>>a;
+        for(int i=0;i<n;i++)a.push_back({ages[i],scores[i]});
+        sort(a.begin(),a.end());
+        vector<int>dp(n,-1);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int sc=a[i].second;
+            dp[i]=a[i].second;
+            for(int j=0;j<i;j++){
+                if(a[j].second<=a[i].second){
+                    dp[i]=max(dp[i],sc+dp[j]);
                 }
             }
+            ans=max(ans,dp[i]);
         }
-
-        int s = 0;
-
-        for(auto it : a){
-            s = max(s, it);
-        }
-
-        return s;
+        return ans;
     }
 };
