493b2724ccf9d9bdf2572bb2f003af7b423f8f1e
Marlapati Nithesh <marlapati.2023ug1112@iiitranchi.ac.in>
2026-08-23 20:18:21 +0530

    Time: 11 ms (43.54%) | Memory: 12.6 MB (44.74%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -4,14 +4,22 @@ public:
         int n=a.size();
         sort(a.begin(),a.end());
         int dp[501][501];
-        memset(dp,-1,sizeof(dp));
-        auto dfs=[&](auto& slef,int idx,int t)->int{
-            if(idx>=n)return 0;
-            if(dp[idx][t]!=-1)return dp[idx][t];
-            int skip=slef(slef,idx+1,t);
-            int taken=a[idx]*t+slef(slef,idx+1,t+1);
-            return dp[idx][t]=max(skip,taken);
-        };
-        return max(0,dfs(dfs,0,1));
-    }
+        memset(dp,0,sizeof(dp));
+        // auto dfs=[&](auto& slef,int idx,int t)->int{
+        //     if(idx>=n)return 0;
+        //     if(dp[idx][t]!=-1)return dp[idx][t];
+        //     int skip=slef(slef,idx+1,t);
+        //     int taken=a[idx]*t+slef(slef,idx+1,t+1);
+        //     return dp[idx][t]=max(skip,taken);
+        // };
+        // for(int i=0;i<501;i++){
+        //     dp[n][i]=0;
+        // }
+        for(int idx=n-1;idx>=0;idx--){
+            for(int t=n;t>=1;t--){
+                dp[idx][t]=max(dp[idx+1][t],a[idx]*t+dp[idx+1][t+1]);
+            }
+        }
+          return max(0,dp[0][1]);
+        }
 };
