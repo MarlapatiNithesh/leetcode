39524d36296dea4da868df2566e7d7a929256ee8
Marlapati Nithesh <marlapati.2023ug1112@iiitranchi.ac.in>
2026-08-26 23:23:31 +0530

    Time: 23 ms (7.23%) | Memory: 22.2 MB (6.25%) - LeetSync

--- 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
@@ -1,79 +1,47 @@
 class Solution {
 public:
-    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+    int minRefuelStops(int target, int startFuel,
+                       vector<vector<int>>& stations) {
+
         int n = stations.size();
 
+        // dp[i][j] = maximum distance reachable
+        // after considering first i stations
+        // using exactly j refuels
         long long dp[501][501];
-        memset(dp, -1, sizeof(dp));
 
-        auto dfs = [&](auto& self, int idx, int cnt) -> long long {
+        for (int i = 0; i <= n; i++)
+            for (int j = 0; j <= n; j++)
+                dp[i][j] = -1;
 
-            if (dp[idx + 1][cnt] != -1)
-                return dp[idx + 1][cnt];
-
-            long long reach;
-
-            if (idx == -1) {
-                reach = startFuel;
-            } else {
-                reach = stations[idx][0] + stations[idx][1];
-            }
-
-            // try all previous possibilities
-            long long ans = reach;
-
-            for (int i = idx + 1; i < n; i++) {
-
-                // station i cannot be reached from current reach
-                if (stations[i][0] > ans)
-                    break;
-
-                ans = max(ans,
-                    self(self, i, cnt + 1)
-                );
-            }
-
-            return dp[idx + 1][cnt] = ans;
-        };
-
-        // Actually simpler and correct recursive DP:
-        vector<vector<long long>> memo(n + 1,
-                                      vector<long long>(n + 1, -1));
-
-        auto solve = [&](auto& self, int idx, int stops) -> long long {
-
-            if (memo[idx + 1][stops] != -1)
-                return memo[idx + 1][stops];
-
-            long long fuel;
-
-            if (idx == -1)
-                fuel = startFuel;
-            else
-                fuel = stations[idx][0] + stations[idx][1];
-
-            return memo[idx + 1][stops] = fuel;
-        };
-
-        // DP: dp[k] = farthest reachable with k stops
-        vector<long long> reach(n + 1, -1);
-        reach[0] = startFuel;
+        dp[0][0] = startFuel;
 
         for (int i = 0; i < n; i++) {
-            for (int j = i; j >= 0; j--) {
 
-                if (reach[j] >= stations[i][0]) {
-                    reach[j + 1] = max(
-                        reach[j + 1],
-                        reach[j] + stations[i][1]
-                    );
+            for (int j = 0; j <= i; j++) {
+
+                if (dp[i][j] == -1)
+                    continue;
+
+                // NOT TAKE
+                dp[i + 1][j] =
+                    max(dp[i + 1][j], dp[i][j]);
+
+                // TAKE
+                if (dp[i][j] >= stations[i][0]) {
+
+                    dp[i + 1][j + 1] =
+                        max(dp[i + 1][j + 1],
+                            dp[i][j] +
+                            stations[i][1]);
                 }
             }
         }
 
-        for (int i = 0; i <= n; i++) {
-            if (reach[i] >= target)
-                return i;
+        for (int j = 0; j <= n; j++) {
+
+            if (dp[n][j] >= target)
+                return j;
         }
 
         return -1;
