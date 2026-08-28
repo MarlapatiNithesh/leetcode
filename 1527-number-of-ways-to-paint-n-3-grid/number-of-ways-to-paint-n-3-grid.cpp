ec367bcc07a709a85653d03b3aa159a3a3c90300
Marlapati Nithesh <marlapati.2023ug1112@iiitranchi.ac.in>
2026-08-28 10:48:29 +0530

    Time: 77 ms (24.66%) | Memory: 18.6 MB (19.41%) - LeetSync

---
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,59 @@
+class Solution{
+public:
+    static const int MOD=1000000007;
+
+    vector<string>gen;
+
+    void generate(string s,int prev){
+        if(s.size()==3){
+            gen.push_back(s);
+            return;
+        }
+
+        for(int i=0;i<3;i++){
+            if(prev==i)continue;
+            generate(s+char('0'+i),i);
+        }
+    }
+
+    int numOfWays(int n){
+
+        generate("",-1);
+
+        int m=gen.size();
+
+        vector<vector<int>>dp(n,vector<int>(m+1,-1));
+
+        auto dfs=[&](auto& self,int idx,int prev)->int{
+            if(idx==n)return 1;
+
+            if(dp[idx][prev+1]!=-1)
+                return dp[idx][prev+1];
+
+            long long ans=0;
+
+            for(int i=0;i<m;i++){
+
+                if(prev!=-1){
+
+                    bool bad=false;
+
+                    for(int j=0;j<3;j++){
+                        if(gen[i][j]==gen[prev][j]){
+                            bad=true;
+                            break;
+                        }
+                    }
+
+                    if(bad)continue;
+                }
+
+                ans=(ans+self(self,idx+1,i))%MOD;
+            }
+
+            return dp[idx][prev+1]=ans;
+        };
+
+        return dfs(dfs,0,-1);
+    }
+};
