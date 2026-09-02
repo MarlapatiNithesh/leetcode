ffb1d5af87c4f31d024943d366bae14afa296d31
Marlapati Nithesh <marlapati.2023ug1112@iiitranchi.ac.in>
2026-09-02 10:55:23 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (67.80%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int n=nums1.size();
+        if(n==1) return true;
+        // int o=0,e=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums1[i]&1)o++;
+        //     else e++;
+        // }
+        // if(o==0 || e==0) return true;
+        // if(o>0) return true;
+        return true;
+    }
+};
