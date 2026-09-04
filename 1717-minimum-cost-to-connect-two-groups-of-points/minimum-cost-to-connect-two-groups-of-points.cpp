9ac3d214d89d4bcf3498a0e8e8bba68ca0404f62
Marlapati Nithesh <marlapati.2023ug1112@iiitranchi.ac.in>
2026-09-04 22:44:09 +0530

    Time: 55 ms (22.27%) | Memory: 11.5 MB (82.59%) - LeetSync

--- 1717-minimum-cost-to-connect-two-groups-of-points/minimum-cost-to-connect-two-groups-of-points.cpp
+++ 1717-minimum-cost-to-connect-two-groups-of-points/minimum-cost-to-connect-two-groups-of-points.cpp
@@ -1,56 +1,37 @@
 class Solution {
 public:
+    int n;
+    int m;
+    int dp[12][1 << 13];
+    int f(int i, int mask, vector<vector<int>>& cost) {
+        if (i == n && (mask == (1 << m) - 1))
+            return 0;
+        
+        if (i == n) {
+            int ans = 0;
+            for (int j = 0; j < m; j++) {
+                if ((mask & (1 << j)) == 0) {
+                    int mn = INT_MAX;
+
+                    for (int k = 0; k < n; k++)
+                        mn = min(mn, cost[k][j]);
+
+                    ans += mn;
+                }
+            }
+            return ans;
+        }
+        if(dp[i][mask]!=-1)return dp[i][mask];
+        int cnt = INT_MAX;
+        for (int j = 0; j < m; j++) {
+            cnt = min(cnt, cost[i][j] + f(i + 1, mask | (1 << j), cost));
+        }
+        return dp[i][mask] = cnt;
+    }
     int connectTwoGroups(vector<vector<int>>& cost) {
-        int n = cost.size();
-        int m = cost[0].size();
-        int done = (1 << m) - 1;
-        int dp[13][13][done+1][2];
-        memset(dp,-1,sizeof(dp));
-        auto dfs = [&](auto& self, int idx1, int idx2,
-                       int mask, int connected) -> int {
-
-            if(idx1 >= n) {
-                if(mask == done)
-                    return 0;
-
-                return INT_MAX;
-            }
-
-            if(idx2 >= m) {
-                if(connected == 0)
-                    return INT_MAX;
-
-                return self(self, idx1 + 1, 0, mask, 0);
-            }
-            if(dp[idx1][idx2][mask][connected]!=-1){
-                return dp[idx1][idx2][mask][connected];
-            }
-
-            int nt = self(
-                self,
-                idx1,
-                idx2 + 1,
-                mask,
-                connected
-            );
-
-            int temp = self(
-                self,
-                idx1,
-                idx2 + 1,
-                mask | (1 << idx2),
-                1
-            );
-
-            int tt = INT_MAX;
-
-            if(temp != INT_MAX) {
-                tt = cost[idx1][idx2] + temp;
-            }
-
-            return dp[idx1][idx2][mask][connected]=min(nt, tt);
-        };
-
-        return dfs(dfs, 0, 0, 0, 0);
+        n = cost.size();
+        m = cost[0].size();
+        memset(dp, -1, sizeof(dp));
+        return f(0, 0, cost);
     }
 };
